b155f799325c80d107185ca2ce79c3793dd0df22
apnicollege <imtiyaz00hirani@gmail.com>
2021-05-01 06:06:35 +0530

    cors for UI

--- src/main/java/com/akeb/CorsConfig.java
+++ src/main/java/com/akeb/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
  import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-	String[] origins = new String[]{"http://localhost:4200"};
+	String[] origins = new String[]{"http://localhost:4200", "https://akebweb.herokuapp.com/" , "akebweb.herokuapp.com/"};
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
