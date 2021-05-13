6b272559cf0baf9f50eafd6a47632f2ddad382b0
apnicollege <imtiyaz00hirani@gmail.com>
2021-05-13 21:06:42 +0530

    council apis added

--- src/main/java/com/akeb/controller/CouncilController.java
+++ src/main/java/com/akeb/controller/CouncilController.java
@@ -1,6 +1,10 @@
 package com.akeb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,13 +25,33 @@ public class CouncilController {
 	private LocalCouncilRepository localCouncilRepository;
 	
 	@PostMapping("/regional-council")
-	public void postRegionalCouncil(@RequestBody RegionalCouncil rc) {
-		regionalCouncilRepository.save(rc);
+	public RegionalCouncil postRegionalCouncil(@RequestBody RegionalCouncil rc) {
+		return regionalCouncilRepository.save(rc);
 	}
 	
 	@PostMapping("/local-council/{regId}")
-	public void postLocalCouncil(@PathVariable("regId") Long regId,@RequestBody LocalCouncil lc) {
+	public LocalCouncil postLocalCouncil(@PathVariable("regId") Long regId,@RequestBody LocalCouncil lc) {
 		lc.setRegionalCouncil(regionalCouncilRepository.getOne(regId));
-		localCouncilRepository.save(lc);
+		return localCouncilRepository.save(lc);
+	}
+	
+	@GetMapping("/regional-council")
+	public List<RegionalCouncil> getRegionalCouncil() {
+		return regionalCouncilRepository.findAll();
+	}
+	
+	@GetMapping("/local-council/{rid}")
+	public List<LocalCouncil> getLocalCouncil(@PathVariable("rid") Long rid) {
+		return localCouncilRepository.findByRegionalCouncil(rid);
+	}
+	
+	@DeleteMapping("/regional-council/{id}")
+	public void deleteRegionalCouncil(@PathVariable("id") Long id) {
+		regionalCouncilRepository.deleteById(id);
+	}
+	
+	@DeleteMapping("/localCouncil/{id}")
+	public void deleteLocalCouncil(@PathVariable("id") Long id) {
+		localCouncilRepository.deleteById(id);
 	}
 }
--- src/main/java/com/akeb/repository/LocalCouncilRepository.java
+++ src/main/java/com/akeb/repository/LocalCouncilRepository.java
@@ -1,9 +1,17 @@
 package com.akeb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.akeb.model.LocalCouncil;
 
 public interface LocalCouncilRepository extends JpaRepository<LocalCouncil, Long>{
+
+	@Query("select l from LocalCouncil l join l.regionalCouncil r where r.id=?1")
+	List<LocalCouncil> findByRegionalCouncil(Long rid);
+
+	 
 
 }
