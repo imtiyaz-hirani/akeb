55bfcd005d4fb278727602b8630e92a0f574f44b
apnicollege <imtiyaz00hirani@gmail.com>
2021-06-09 17:07:50 +0530

    cors firebase access pass

--- src/main/java/com/akeb/CorsConfig.java
+++ src/main/java/com/akeb/CorsConfig.java
@@ -5,7 +5,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
  import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-	String[] origins = new String[]{"http://localhost:4200", "localhost"};
+	String[] origins = new String[]{"http://localhost:4200", "localhost","https://akeb-reg.web.app", 
+			"akeb-reg.web.app"};
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
